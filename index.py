8b70c1a856bcfdc865ec26ebb1ee6732608063dc
TaberFast <duberfasttop2018@gmail.com>
2023-05-25 17:37:05 +0500

    1.1
    asdasd

--- index.py
+++ index.py
@@ -26,9 +26,9 @@ questions = [
     'Вы клуб предугадывающих шутки?',
     'че?',
     'Ваши родители работают в телефонной компании',
-    'Вы фанат нинтендо свич? (ответишь да я тебя найду и вырежу твою семью)',
-    'фыолофылдполыиполывалоыдвра',
-    'да'
+    'Вы фанат нинтендо свич? (ответишь да я тебя найду и твою сине-красную консоль выкину в окно)',
+    'Готовы к тому то что вашу картофельную подругу захавает птица?',
+    'Вам нужен пульт от ядерки?'
 ]
 
 #Create and setup app
@@ -54,13 +54,18 @@ def rediska(question, btntxt):
     global text
     global button
     global count
-    text.setText(question)
-    button.setText(btntxt)
+    global vlayout1
+    if count == 19 :
+        #mw.hide
+        text.setText('ПОЗДРАВЛЯЕМ ВЫ НА ВСЕ 100% ГЛАВНЫЙ ПЕРСОНАЖ ИГРЫ "Far Cry New Dawn"!!!!!!!!')
+        vlayout1.removeWidget(button)
+        sip.delete(button)
+    else:
+        text.setText(question)
+        button.setText(btntxt)
     count += 1
-    
 button.clicked.connect(lambda:rediska(questions[count], 'да'))
-if count == 19 :
-    mw.hide
+
 
 mw.show()
 app.exec_()
